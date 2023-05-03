96a32a86422041530d416f464377b6af7b7d3e94
qa-hetmu-13-ughw@example.com <qa-hetmu-13-ughw@example.com>
2023-05-03 12:34:34 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16328.stoplight-dev.com/api/v1/projects/cHJqOjI1MA/spectral.js?branch_slug=main&token=f8003d2e-d982-4c99-a426-948244500593";
+export default { extends: ruleset };
